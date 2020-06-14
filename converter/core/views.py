fb393e3a44049db95da58e2522c668415526d35d
ivan1994petrov <ivan94hristov@gmail.com>
2020-06-14 15:32:32 +0300

    use ajax and make some improvements

--- converter/core/views.py
+++ converter/core/views.py
@@ -1,8 +1,7 @@
 from django.views import View
 from django.views.generic import ListView
-from django.http import HttpResponse, JsonResponse
+from django.http import JsonResponse
 from django.shortcuts import render
-from django.core.exceptions import ObjectDoesNotExist
 from django.core.management import call_command
 
 from core.models import BaseCurrency, QuoteCurrency, CurrencyPair
@@ -25,33 +24,35 @@ class Calculator(View):
 
         return render(request, 'pages/calculator.html', context)
 
-    def post(self, request):
-        post_data = request.POST
-        from_currency = post_data.get('from_currency')
-        to_currency = post_data.get('to_currency')
-        amount = post_data.get('amount')
-
-        try:
-            item = CurrencyPair.objects.get(
-                base_currency=from_currency,
-                quote_currency=to_currency)
-            quote = item.quote
-            result = float(amount) * float(quote)
-            return HttpResponse(f'{amount}*{quote}={result}')
-        except ObjectDoesNotExist:
-            # if I don't have this pair into the DB
-
-            items = CurrencyPair.objects.filter(
-                base_currency=from_currency)
-            base_currency_obj = BaseCurrency.objects.get(
-                pk=from_currency)
-            context = {
-                'base_currency_obj': base_currency_obj,
-                'items': items,
-            }
-            return render(request, 'pages/missing_pair.html', context)
-
 
 def ajax_sync_now(request):
     call_command('extract_the_current_course')
     return JsonResponse({})
+
+
+def ajax_calculator(request):
+    base_currency = request.GET.get('fromCurrencyValue', None)
+    quote_currency = request.GET.get('toCurrency', None)
+    amount = request.GET.get('amount', None)
+    try:
+        currency_pair = CurrencyPair.objects.get(
+            base_currency=base_currency,
+            quote_currency=quote_currency,
+        )
+        quote = currency_pair.quote
+        result = float(quote) * float(amount)
+
+        data = {
+            'result': result
+        }
+        return JsonResponse(data)
+    except:
+        currency_pairs = CurrencyPair.objects.filter(
+            base_currency=base_currency
+        )
+        result = [currency_pair.quote_currency.quote_currency for
+                  currency_pair in currency_pairs]
+        data = {
+            'result': result
+        }
+        return JsonResponse(data)
